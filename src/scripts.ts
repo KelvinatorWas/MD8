12332070e9e643ec53071763420ad5e17f2afb63
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-11-21 18:22:59 +0200

    reading data

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,36 @@
-import $ from 'jquery';
-import sum from './utils/sum/sum';
+import axios from 'axios';
 
-console.log('Ready for coding');
+const idWrapper = document.querySelector('.id-wrapper');
 
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
+// create a basic Element
+const createElement = (type = 'div', cssClass = '', jsonIdentifierClass = ''):HTMLElement => {
+  const element = document.createElement(type);
+  element.className = cssClass;
+  if (jsonIdentifierClass.length) element.classList.add(jsonIdentifierClass);
+  return element;
+};
+
+// Type for all the data types in IDD database
+type IID = {
+    id: number,
+    name: string,
+    species: string,
+    sex: string,
+    occupation: string,
+    origin: string,
+    pfp: string,
+    birth_year: string,
+    alive: boolean,
+    desc: string,
+}
+
+const result = axios.get<IID[]>('http://localhost:3004/IID');
+
+let IIDs:IID[] = [];
+
+result.then(({ data }) => {
+  data.forEach((id) => {
+    const idCard = createElement(undefined, 'iid-card', 'iid-card-wrapper');
+    idWrapper.appendChild(idCard);
+  });
+});
