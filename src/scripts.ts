7ebf9b5e054fb9c306702c30ab2cff96f6899008
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-11-21 22:22:52 +0200

    some refactoring

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,36 +1,86 @@
 import axios from 'axios';
 
-const idWrapper = document.querySelector('.id-wrapper');
+// Type for all the data types in IDD database
+type IID = {
+  id: number;
+  name: string;
+  species: string;
+  sex: string;
+  occupation: string;
+  origin: string;
+  pfp: string;
+  birth_year: string;
+  status: string;
+  desc: string;
+};
 
-// create a basic Element
-const createElement = (type = 'div', cssClass = '', jsonIdentifierClass = ''):HTMLElement => {
+const createElement = (type = 'div', cssClass = '', jsonIdentifierClass = ''): HTMLElement => {
   const element = document.createElement(type);
   element.className = cssClass;
   if (jsonIdentifierClass.length) element.classList.add(jsonIdentifierClass);
   return element;
 };
 
-// Type for all the data types in IDD database
-type IID = {
-    id: number,
-    name: string,
-    species: string,
-    sex: string,
-    occupation: string,
-    origin: string,
-    pfp: string,
-    birth_year: string,
-    alive: boolean,
-    desc: string,
+const createTextElement = (parent:HTMLElement, type = 'div', cssClass = '', text = 'Hello') => {
+  const textElement = createElement(type, cssClass);
+  textElement.textContent = text;
+  parent.appendChild(textElement);
+};
+
+class IDCardManager {
+  private idWrapper: HTMLElement | null;
+
+  constructor() {
+    this.idWrapper = document.querySelector('.id-wrapper');
+  }
+
+  private createCardMainData(idCard: HTMLElement, data: IID): void {
+    const mainDataWrapper = createElement(undefined, 'iid-card__main-data-wrapper');
+    const pfp = createElement(undefined, 'iid-card___pfp');
+    const dataRect = createElement('div', 'iid-main-data-wrapper__data-rect');
+    const bottomCardDataRect = createElement('div', 'iid-card-data-rect');
+
+    // Add Top Rect Data Text Elements
+
+    createTextElement(dataRect, 'p', 'iid-main-data-wrapper__text', `Name: ${data.name}`);
+    createTextElement(dataRect, 'p', 'iid-main-data-wrapper__text', `Species: ${data.species}`);
+    createTextElement(dataRect, 'p', 'iid-main-data-wrapper__text', `Sex: ${data.sex}`);
+    createTextElement(dataRect, 'p', 'iid-main-data-wrapper__text', `Origin: ${data.origin}`);
+
+    // Add Bottom Rect Data Text Elements
+
+    createTextElement(bottomCardDataRect, 'p', 'iid-main-data-wrapper__text', `Occupation: ${data.occupation}`);
+    createTextElement(bottomCardDataRect, 'p', 'iid-main-data-wrapper__text', `Birth Year: ${data.birth_year} ${data.origin}'s Years`);
+    createTextElement(bottomCardDataRect, 'p', 'iid-main-data-wrapper__text', `Satus: ${data.status}`);
+    createTextElement(bottomCardDataRect, 'p', 'iid-main-data-wrapper__text', `Origin: ${data.origin}`);
+
+    // Add the Data to the IdCard
+    mainDataWrapper.appendChild(pfp);
+    mainDataWrapper.appendChild(dataRect);
+
+    idCard.appendChild(mainDataWrapper);
+    idCard.appendChild(bottomCardDataRect);
+  }
+
+  fetchAndDraw(): void {
+    const result = axios.get<IID[]>('http://localhost:3004/IID');
+
+    result.then(({ data }) => {
+      data.forEach((id) => {
+        const idCard = createElement(undefined, 'iid-card', 'js-card-wrapper');
+
+        this.createCardMainData(idCard, id);
+
+        if (this.idWrapper) {
+          this.idWrapper.appendChild(idCard);
+        }
+      });
+    });
+  }
 }
 
-const result = axios.get<IID[]>('http://localhost:3004/IID');
+// Create an instance of the class
+const idCardManager = new IDCardManager();
 
-let IIDs:IID[] = [];
-
-result.then(({ data }) => {
-  data.forEach((id) => {
-    const idCard = createElement(undefined, 'iid-card', 'iid-card-wrapper');
-    idWrapper.appendChild(idCard);
-  });
-});
+// Fetch data and render cards
+idCardManager.fetchAndDraw();
